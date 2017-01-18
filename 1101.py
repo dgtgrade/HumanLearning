733d37dd0964cbe5d7bb4ac6e3c7ed131a303c70
dgtgrade <dgtgrade@gmail.com>
2017-01-18 14:23:57 +0900

    다른 함수들도 되는 버전으로 완성

--- 1101.py
+++ 1101.py
@@ -3,24 +3,41 @@ import numpy as np
 #
 RAND_MAX = 1
 
-# train set
-train_x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-train_y = np.array([[0], [1], [1], [0]])
+# train set: XOR
+# train_x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+# train_y = np.array([[0], [1], [1], [0]])
+
+# train set: unknown function
+# train_x = np.array([0, 1, 2, 5, 9, 11, 15])
+# train_y = np.array([0, 1, 0, 1, 1, 1, 0])
+
+# train set: SIN function
+train_x = np.array([0, 1, 2, 5, 9, 11, 15])
+train_y = np.sin(train_x)
+
+#
+train_x = train_x.reshape(len(train_x), -1)
+train_y = train_y.reshape(len(train_x), -1)
 
 # number of train examples
 m = train_x.shape[0]
 
 # number of nodes for each layers
 n_input_layer = train_x.shape[1]
-n_hidden_layer1 = 2
-n_hidden_layer2 = 2
+n_hidden_layer = 30
 n_output_layer = train_y.shape[1]
 
 
 def threshold(z: np.ndarray):
     return (z > 0.5).astype(np.float)
 
-activate = threshold
+
+def sigmoid(z: np.ndarray):
+    return 1 / (1 + np.exp(-z))
+
+
+# activate = threshold
+activate = sigmoid
 
 
 def add_bias(a: np.ndarray):
@@ -32,7 +49,7 @@ epoch = 0
 while True:
 
     epoch += 1
-    pred_y = np.empty(train_y.shape)
+    preds = np.empty(train_y.shape)
     loss = 0
 
     w0 = None
@@ -44,40 +61,38 @@ while True:
         # input layer
         input_layer = train_x[i]
 
-        # connections between input layer and hidden layer1
-        w0 = np.random.random(size=(n_input_layer + 1, n_hidden_layer1))
+        # connections between input layer and hidden layer
+        w0 = np.random.random(size=(n_input_layer + 1, n_hidden_layer))
         w0 = (w0 * 2 - 1) * RAND_MAX
 
-        # hidden layer1
-        hidden_layer1_z = np.dot(add_bias(input_layer), w0)
-        hidden_layer1_a = activate(hidden_layer1_z)
+        # hidden layer
+        hidden_layer_z = np.dot(add_bias(input_layer), w0)
+        hidden_layer_a = activate(hidden_layer_z)
 
-        #
-        w1 = np.random.random(size=(n_hidden_layer1 + 1, n_hidden_layer2))
+        # connections between hidden layer and output layer
+        w1 = np.random.random(size=(n_hidden_layer + 1, n_output_layer))
         w1 = (w1 * 2 - 1) * RAND_MAX
 
-        #
-        hidden_layer2_z = np.dot(add_bias(hidden_layer1_z), w1)
-        hidden_layer2_a = activate(hidden_layer2_z)
-
-        #
-        w2 = np.random.random(size=(n_hidden_layer2 + 1, n_output_layer))
-        w2 = (w2 * 2 - 1) * RAND_MAX
-
         # output layer
-        output_layer_z = np.dot(add_bias(hidden_layer2_a), w2)
+        output_layer_z = np.dot(add_bias(hidden_layer_a), w1)
         output_layer_a = activate(output_layer_z)
 
+        # y_pred = threshold(output_layer_a)
+        y_pred = np.array(output_layer_a)
+
         #
-        pred_y[i] = output_layer_a
-        loss += np.sum((output_layer_z - train_y) ** 2)
+        y_true = train_y[i]
+
+        #
+        loss += np.sum((y_pred - y_true) ** 2)
+        preds[i] = y_pred
 
         print("epoch #: {}, example #: {}, x: {}, y_true: {}, y: {}".format(
-            epoch, i, train_x[i], train_y[i], output_layer_a))
+            epoch, i, train_x[i], y_true, y_pred))
 
-    correct_prediction = np.sum(pred_y == train_y) / m
+    correct_prediction = np.sum(preds == train_y) / m
 
-    print("epoch #: {}, correct result: {:5.3f}%, loss: {:10.3f}".format(
+    print("epoch #: {}, correct result: {:5.3f}%, loss: {:6.3f}".format(
         epoch, correct_prediction * 100, loss))
 
     if correct_prediction == 1.0:
@@ -85,7 +100,5 @@ while True:
         print(w0)
         print("w1:")
         print(w1)
-        print("w2:")
-        print(w2)
         break
 
